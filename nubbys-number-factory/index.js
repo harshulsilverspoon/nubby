c8bb21502f42a74162fc7ea17a4eef24f6c3f86b
harshulsilverspoon <chucklesgalore2017@gmail.com>
2026-03-12 19:21:40 +1100

    Update index.js

--- nubbys-number-factory/index.js
+++ nubbys-number-factory/index.js
@@ -124,7 +124,7 @@
       Module.setStatus("Downloading...");
       window.onerror = function (event) {
         // TODO: do not warn on ok events like simulating an infinite loop or exitStatus
-        Module.setStatus("Exception thrown, see JavaScript console");
+        Module.setStatus("");
         spinnerElement.style.display = "none";
         Module.setStatus = function (text) {
           if (text) Module.printErr("[post-exception status] " + text);
